ff0d64c0d728c04708552a0e605422fbd05015a1
Daniel Ruales <danielmruales@gmail.com>
2019-03-31 12:26:14 -0600

    initial layout of projects page

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Navbar from '../Navbar/Nabar'
+import './Projects.css'
 
 const Projects = () => {
     return (
         <div>
         <Navbar/>
-        <h1>It's me</h1>
+        <div className='mainProjectsDiv'>
+            <a href='http://wanderlustbase.surge.sh/' target='_blank' rel="noopener noreferrer" >
+                <img src='https://warnercnr.colostate.edu/wp-content/uploads/sites/2/2017/04/shutterstock_428626417-1024x683.jpg' alt='preview' className='projects'/>
+            </a>
+            <img src='https://www.tripsavvy.com/thmb/IXSDIHwCLwuwxUHJhlHyd1L4jpA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-547918233-5907d0305f9b58647026e62a.jpg' alt='preview' className='projects' />
+            <img src='http://www.adventureprofessionals.com.au/wp-content/uploads/2014/03/AP-Adventure-on-High.jpg' className='projects' alt='preview' />
+        </div>
         </div>
     );
 };
