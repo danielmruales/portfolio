4eac24a32a985d54e349fafc3ed7fc9a7c418d69
Daniel Ruales <danielmruales@gmail.com>
2019-03-31 11:42:28 -0600

    changed what pages navbar goes on

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 import Home from './Home/Home';
-import Navbar from './Navbar/Nabar'
+// import Navbar from './Navbar/Nabar'
+import Projects from './Projects/Projects'
 import Footer from './Footer/Footer'
 import './App.css';
 
@@ -9,11 +10,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar/>
         <Switch>
           <Route exact path='/' component={Home}/>
-          {/* <Route path='/projects' component={Projects}/>
-          <Route path='/about' component={About}/>
+          <Route path='/projects' component={Projects}/>
+          {/* <Route path='/about' component={About}/>
           <Route path='/contact' component={Contact}/> */}
         </Switch>
         <Footer/>
---
+++ src/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/Nabar'
+
+const Projects = () => {
+    return (
+        <div>
+        <Navbar/>
+        <h1>It's me</h1>
+        </div>
+    );
+};
+
+export default Projects;
