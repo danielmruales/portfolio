ed6cb0f6e54100716edd111350d6ffac3d8b2a21
Daniel Ruales <danielmruales@gmail.com>
2019-09-24 01:14:06 -0600

    test

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Navbar from '../Navbar/Nabar'
+// import Navbar from '../Navbar/Nabar'
 import './Contact.css'
 
 const Contact = () => {
     return (
         <div>
-            <Navbar/>
+            {/* <Navbar/> */}
             <div className='container'>
                 <h1 className='message'>Let's Chat!</h1>
                 
-                <h3>Want to collaborate on a project? Shoot me an email at <span> danielmruales@gmail.com</span></h3>
+                <h3>Want to collaborate on a project? Shoot me an email <span> danielmruales@gmail.com</span></h3>
                 
             </div>
         </div>
