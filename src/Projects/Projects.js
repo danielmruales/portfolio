d77f54002c83efcfabbe80f7f758d077982799ad
Daniel Ruales <danielmruales@gmail.com>
2020-04-30 01:57:59 -0600

    updated resume, fixed grammatical errors

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
                 <img src={evas} alt='project screenshot' className='projects'/>
                 <p className='description'>Full Stack website for a local bakery</p>
                 <p className='madeWith'> React, Node, MongoDB, Express </p>
-                <p className='firstBtn'> View On GitHub</p>
+                <p className='firstBtn'> View on GitHub</p>
             </a>
             <a href='https://wanderlusthikes.netlify.app' target='_blank' rel="noopener noreferrer" >
                 <h1 className='projectTitle'>WanderLust</h1>
@@ -31,14 +31,14 @@ const Projects = () => {
                 <img src={philosophy} className='philosophyPic' alt='preview' />
                 <p className='description'>Website aimed at democratizing philosophy</p>
                 <p className='madeWith'> React, Node, JavaScript </p>
-                <p className='lastLiveBtn'>View Live</p>
+                <p className='lastLiveBtn'>View on GitHub</p>
             </a>
             <a href='https://github.com/danielmruales/mccauleyfinnegan' target='_blank' rel="noopener noreferrer">
                 <h1 className='projectTitle' >M.C. Finnegan</h1>
                 <img src={fin} className='projects' alt='preview' />
                 <p className='description'>Full Stack website for local professional</p>
                 <p className='madeWith'> React, Node, MongoDB, Express </p>
-                <p className='caulBtn'>View On GitHub</p>
+                <p className='caulBtn'>View on GitHub</p>
             </a>
             
         </div>
