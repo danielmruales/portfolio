7d6b7cb4184c296ac965dc20c13be7284e87c18d
Daniel Carrera <danielmruales@gmail.com>
2020-05-25 18:16:14 -0600

    changed opening message

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div className='home'>
             <h1 className='hi'>Hi, I'm </h1>
             <h1 className='title'>Daniel Ruales</h1>
-            <h2 className='subTitle'>Product Manager & Front-End Web Developer</h2>
+            <h2 className='subTitle'>Product Manager & Web Developer</h2>
             <div className='buttonDiv'>
                 <Link to='/resume' className='button'> View My Resume </Link>
                 <Link to='/about' className='aboutBtn'> A Bit About Me </Link>
