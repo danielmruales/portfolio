2ba02d1551a75bcf3ba6a52e564519beca728018
Daniel Ruales <danielruales@Daniel-Ruales.local>
2022-05-05 16:50:41 -0600

    updated text throught the site

--- src/About/About.js
+++ src/About/About.js
@@ -12,10 +12,10 @@ const About = () => {
             </div>
                 <p className='aboutMe'>
                     I am a <span className="adjectives">passionate</span> , <span className="adjectives">driven</span>, and <span className="adjectives"> adaptable</span> learner. 
-                    Working on products that help bridge the gap between humnans and technology is what I do. 
+                    Nothing excites me more than working on and buildoing software that helps bridge the gap between humans and technology. 
                     <br/>
                     <br/>
-                    Some of my passions in life include visiting the symphony, playing soccer, Taekwondo and philosophy. 
+                    Some of my passions in life outside of the tech industry include visiting the symphony, soccer, Taekwondo, Magic the Gathering, and philosophy. 
                     {/* As a black belt at <span href="https://www.paceacademytkd.com"> Pace Academy Taekwondo </span> */}
                 </p>
 
