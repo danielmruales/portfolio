b627411c4cb365f22dac29bf8eedf2252c570937
Daniel Ruales <danielruales@Daniel-Ruales.local>
2022-05-05 15:18:48 -0600

    testing connection

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
         <div className='home'>
             <h1 className='hi'>Hi, I am </h1>
             <h1 className='title'>Daniel Ruales</h1>
-            <h2 className='subTitle'>Product Manager, Developer, and SDR </h2>
+            <h2 className='subTitle'>Product Specialist </h2>
             {/* <h2 className='subTitle'>Product, engineering, sales is what I do</h2> */}
             <div className='buttonDiv'>
                 {/* <Link to='/projects' className='button'> View Projects </Link>  */}
-                <Link to='/about' className='aboutBtn'> Get to know me </Link>
+                <Link to='/about' className='aboutBtn'> Get to know me! </Link>
             </div>
         </div>
 
